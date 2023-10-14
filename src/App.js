40e9a7767e94d17aefdc147d0f3d96260540beef
dawncoding <sunnuri99@gmail.com>
2023-10-14 09:31:39 +0900

    feat/#2 - 포물선 생성
    애니메이션 구현 보류

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./styles/css/common/Container.css";
 import "./styles/css/common/Reset.css";
 import "./assets/font/Font.css";
 import Header from "./components/Header";
+import ParabolaBackground from "./components/ParabolaBackground";
 
 function App() {
   return (
     <div className="container">
       <div className="container__header">
         <Header />
+      </div>
+      <div className="container__body">
+        <ParabolaBackground />
       </div>
     </div>
   );
---
+++ src/components/ParabolaBackground.js
@@ -0,0 +1,10 @@
+import "../styles/css/common/ParabolaBackground.css";
+import vector from "../assets/image/vector.png";
+
+export default function ParabolaBackground() {
+  return (
+    <div className="parabola">
+      <img src={vector}></img>
+    </div>
+  );
+}
