88c0b7345a798c9f6c603cbb42f0f074148e18a3
dawncoding <sunnuri99@gmail.com>
2023-10-18 10:01:25 +0900

    feat/#3 - info container 구성 완료

---
+++ src/components/Introduction.js
@@ -0,0 +1,10 @@
+import "../styles/css/common/Introduction.css";
+
+export default function Introduction() {
+  return (
+    <div className="intro">
+      <div className="intro__title">Intro</div>
+      <div className="intro__content">자기소개 내용</div>
+    </div>
+  );
+}
