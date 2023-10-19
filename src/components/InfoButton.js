7f28827449c74919d1ad82d693f41a0981249ccd
dawncoding <sunnuri99@gmail.com>
2023-10-19 09:30:55 +0900

    feat/#8 - info 버튼(깃허브, 블로그) 동작 구현

--- src/components/InfoButton.js
+++ src/components/InfoButton.js
@@ -7,15 +7,34 @@ export default function InfoButton() {
   useEffect(() => {
     AOS.init({ duration: 1200 });
   }, []);
+
+  // GitHub 버튼 클릭 시 이벤트 핸들러
+  const handleGitHubClick = () => {
+    window.open("https://github.com/dawncoding", "_blank");
+  };
+
+  // Blog 버튼 클릭 시 이벤트 핸들러
+  const handleBlogClick = () => {
+    window.open("https://dawnwithsun.tistory.com/", "_blank");
+  };
+
   return (
     <div className="info__button">
       <button className="info__button--email" data-aos="fade-left">
         sunnuri99@gmail.com
       </button>
-      <button className="info__button--github" data-aos="fade-left">
+      <button
+        className="info__button--github"
+        data-aos="fade-left"
+        onClick={handleGitHubClick}
+      >
         GITHUB
       </button>
-      <button className="info__button--blog" data-aos="fade-left">
+      <button
+        className="info__button--blog"
+        data-aos="fade-left"
+        onClick={handleBlogClick}
+      >
         BLOG
       </button>
       <button className="info__button--resume" data-aos="fade-left">
