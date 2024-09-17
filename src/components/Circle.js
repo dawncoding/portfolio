dd64dd7931bb9e6a027595efb3251431f769b23a
dawncoding <sunnuri99@gmail.com>
2024-09-17 15:59:48 +0900

    chore: remove components for refactoring portfolio

--- src/components/Circle.js
+++
@@ -1,5 +0,0 @@
-import "../styles/css/common/Circle.css";
-
-export default function Circle({ circleNumber }) {
-  return <div className={`circle ${circleNumber}`}></div>;
-}
--- src/components/SkillFilter.js
+++
@@ -1,9 +0,0 @@
-import "../styles/css/common/Skill.css";
-
-export default function SkillFilter(props) {
-  return (
-    <>
-      <div className="skill__filter--box">{props.name}</div>
-    </>
-  );
-}
