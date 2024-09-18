2dd4ebbe6998ceb1dfdfdf87792de5d43a33560c
dawncoding <sunnuri99@gmail.com>
2024-09-18 20:09:30 +0900

    feat: create portfolio layout and add Header, BusinessCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
-import theme from "@/styles/theme.ts";
-import { Box } from "@mui/material";
+'use client';
+
+import BusinessCard from '@/components/BusinessCard/index.tsx';
+import Header from '@/components/Header/index.tsx';
+import theme from '@/styles/theme.ts';
+import { Box, Stack, useMediaQuery } from '@mui/material';
 
 export default function Home() {
-  return <Box sx={{ backgroundColor: theme.palette.background.default }}></Box>;
+  const isMediumSmaller = useMediaQuery(theme.breakpoints.down('md'));
+  return (
+    <Box
+      sx={{ backgroundColor: theme.palette.background.default }}
+      display="flex"
+      justifyContent="center"
+    >
+      <Stack width="100%" maxWidth="87.5rem">
+        <Header />
+        <Box
+          display="flex"
+          justifyContent={isMediumSmaller ? 'center' : 'start'}
+        >
+          <BusinessCard />
+        </Box>
+      </Stack>
+    </Box>
+  );
 }
