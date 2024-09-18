c92d8d3e55bbe3f153f2c52556883bf9c8248b67
dawncoding <sunnuri99@gmail.com>
2024-09-18 16:42:47 +0900

    feat(theme): add extended palette and custom typography
    - Extended MUI Palette to include a custom gray color palette
      - Main color: #66717E
      - Light color: #D9D9D9
    - Used `augmentColor` to define custom gray palette
    - Set background color to #ACE4C9
    - Added 'Gowun Batang', 'DM Serif Display', 'Arial', and 'sans-serif' to typography fontFamily

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,37 @@
-import { createTheme } from "@mui/material";
+import { createTheme, PaletteColor } from '@mui/material';
 
-const theme = createTheme({
+// MUI의 Palette와 PaletteOptions 타입 확장
+declare module '@mui/material/styles' {
+  interface Palette {
+    gray: PaletteColor; // Palette에 gray 추가
+  }
+  interface PaletteOptions {
+    gray?: PaletteColor; // PaletteOptions에 gray 추가
+  }
+}
+
+let theme = createTheme();
+
+theme = createTheme(theme, {
   palette: {
     background: {
-      default: "#ACE4C9",
+      default: '#ACE4C9',
     },
+    gray: theme.palette.augmentColor({
+      color: {
+        main: '#66717E',
+        light: '#D9D9D9',
+      },
+      name: 'gray',
+    }),
   },
   typography: {
-    fontFamily: ["DMSerifDisplay", "GowunBatang"].join(","),
+    fontFamily: [
+      '"Gowun_Batang"',
+      '"DM_Serif_Display"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
   },
 });
 
