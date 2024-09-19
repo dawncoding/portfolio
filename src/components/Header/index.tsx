8836768c59eef251b71244d87dafd80d38367d2a
dawncoding <sunnuri99@gmail.com>
2024-09-20 01:17:28 +0900

    feat: create Drawer component in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,12 @@ import BoxShadowContainer from '@/components/BoxShadowContainer/index.tsx';
 import {
   Box,
   Button,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
   Stack,
   useMediaQuery,
   useTheme,
@@ -12,10 +17,36 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import Text from '@/components/Text/index.tsx';
 import { dmSerifDisplay } from '@/styles/font.ts';
+import React from 'react';
 
 const Header: React.FC = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
   const theme = useTheme();
   const isMediumSmaller = useMediaQuery(theme.breakpoints.down('md'));
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setIsDrawerOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: '40%' }} onClick={toggleDrawer(false)}>
+      <List>
+        {[
+          'Intro',
+          'Skill',
+          'Career',
+          'Team Projects',
+          'Personal Experience',
+        ].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <BoxShadowContainer borderRadius="2.5rem" padding="1.5rem" margin="1rem">
@@ -26,14 +57,20 @@ const Header: React.FC = () => {
           </Text>
         </Box>
         {isMediumSmaller ? (
-          <IconButton
-            sx={{
-              color: theme.palette.common.black,
-              padding: 0,
-            }}
-          >
-            <MenuIcon sx={{ width: '2rem', height: '2rem' }} />
-          </IconButton>
+          <>
+            <IconButton
+              sx={{
+                color: theme.palette.common.black,
+                padding: 0,
+              }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon sx={{ width: '2rem', height: '2rem' }} />
+            </IconButton>
+            <Drawer open={isDrawerOpen} onClose={toggleDrawer(false)}>
+              {DrawerList}
+            </Drawer>
+          </>
         ) : (
           <Stack direction="row" gap="1rem">
             <Button
