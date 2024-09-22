c2dd82a17288e74de82a1ee5810f4bf7b258f48a
dawncoding <sunnuri99@gmail.com>
2024-09-22 22:58:41 +0900

    feat: add yellow and green color to MUI theme palette

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,12 +3,16 @@ import { createTheme, PaletteColor } from '@mui/material';
 // MUI의 Palette와 PaletteOptions 타입 확장
 declare module '@mui/material/styles' {
   interface Palette {
-    gray: PaletteColor; // Palette에 gray 추가
+    yellow: PaletteColor;
+    green: PaletteColor;
     blue: PaletteColor;
+    gray: PaletteColor;
   }
   interface PaletteOptions {
-    gray?: PaletteColor; // PaletteOptions에 gray 추가
+    yellow?: PaletteColor;
+    green?: PaletteColor;
     blue?: PaletteColor;
+    gray?: PaletteColor;
   }
 }
 
@@ -16,15 +20,17 @@ let theme = createTheme();
 
 theme = createTheme(theme, {
   palette: {
-    background: {
-      default: '#ACE4C9',
-    },
-    gray: theme.palette.augmentColor({
+    yellow: theme.palette.augmentColor({
       color: {
-        main: '#66717E',
-        light: '#D9D9D9',
+        main: '#F5FADB',
       },
-      name: 'gray',
+      name: 'yellow',
+    }),
+    green: theme.palette.augmentColor({
+      color: {
+        main: '#ACE4C9',
+      },
+      name: 'green',
     }),
     blue: theme.palette.augmentColor({
       color: {
@@ -33,6 +39,13 @@ theme = createTheme(theme, {
         dark: '#377BCB',
       },
       name: 'blue',
+    }),
+    gray: theme.palette.augmentColor({
+      color: {
+        main: '#66717E',
+        light: '#D9D9D9',
+      },
+      name: 'gray',
     }),
   },
   typography: {
