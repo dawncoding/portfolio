c15c2f9c3bb763e5254500a7cf55938dcd652f14
dawncoding <sunnuri99@gmail.com>
2024-09-29 01:45:00 +0900

    feat(layout): add title tag to RootLayout for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import theme from '@/styles/theme.ts';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
@@ -10,6 +11,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Head>
+        <title>김선진 포트폴리오</title>
+      </Head>
       <body>
         <ThemeProvider theme={theme}>
           <CssBaseline />
